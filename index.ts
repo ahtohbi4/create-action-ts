d598f89a8a12ac487dcab6ede27877e66f3943a8
Alexander Antonov <alexandr-post@yandex.ru>
2019-02-08 15:11:49 +0300

    Script was created

---
+++ index.ts
@@ -0,0 +1,26 @@
+interface Action<T extends string> {
+    type: T,
+}
+
+interface ActionWithPayload<T extends string, P> extends Action<T> {
+    payload: P,
+}
+
+interface ActionWithMeta<T extends string, P, M> extends ActionWithPayload<T, P> {
+    meta: M,
+}
+
+function createAction<T extends string>(type: T): Action<T>;
+function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
+function createAction<T extends string, P, M>(type: T, payload: P, meta: M): ActionWithMeta<T, P, M>;
+
+function createAction<T extends string, P, M>(type: T, payload?: P, meta?: M) {
+    return {
+        type,
+
+        ...(meta ? { meta } : {}),
+        ...(payload ? { payload } : {}),
+    };
+}
+
+export default createAction;
