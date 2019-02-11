ff43c96479d045ad965328208f0b58c9a452f542
Alexander Antonov <alexandr-post@yandex.ru>
2019-02-11 14:17:22 +0300

    tests were created

---
+++ test/index.ts
@@ -0,0 +1,74 @@
+import 'mocha';
+import { expect } from 'chai';
+
+import createAction from '../src';
+
+enum ActionKeys {
+    SIMPLE = 'simpleAction',
+    WITH_PAYLOAD = 'actionWithPayload',
+    WITH_META = 'actionWithMeta',
+}
+
+describe('The function createAction()', () => {
+    it('should return the action creator for a simple Action', () => {
+        const actionCreator = () => createAction(ActionKeys.SIMPLE);
+
+        expect(actionCreator()).to.deep.equal({ type: 'simpleAction' });
+    });
+
+    it('should return the action creator for an Action with payload', () => {
+        const action = (payload: number) => createAction(ActionKeys.WITH_PAYLOAD, payload);
+
+        expect(action(2)).to.deep.equal({
+            payload: 2,
+            type: 'actionWithPayload',
+        });
+    });
+
+    it('should return the action creator for an Action with meta', () => {
+        const actionCreator = (meta: object) => createAction(ActionKeys.WITH_META, null, meta);
+        const action = actionCreator({
+            form: 'some-form',
+            field: 'some-field',
+        });
+
+        expect(action).to.deep.equal({
+            meta: {
+                form: 'some-form',
+                field: 'some-field',
+            },
+            type: 'actionWithMeta',
+        });
+    });
+
+    it('should return the action creator for an Action with meta and payload', () => {
+        interface Meta {
+            entity: string,
+        }
+        interface Payload {
+            data: Array<object>,
+            page: number,
+            pages: number,
+        }
+        const actionCreator = (payload: Payload, meta: Meta) => createAction(ActionKeys.WITH_META, payload, meta);
+        const action = actionCreator({
+            data: [],
+            page: 1,
+            pages: 1,
+        }, {
+            entity: 'pokemons',
+        });
+
+        expect(action).to.deep.equal({
+            meta: {
+                entity: 'pokemons',
+            },
+            payload: {
+                data: [],
+                page: 1,
+                pages: 1,
+            },
+            type: 'actionWithMeta',
+        });
+    });
+});
